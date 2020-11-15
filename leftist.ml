5b50fe93868e99538e013b8a81f8f155cdeb5b4e
barteksad <barteksadlej@gmail.com>
2020-11-15 19:27:58 +0100

    first working version

--- leftist.ml
+++ leftist.ml
@@ -1,12 +1,8 @@
-type 'a node = 
-    Node of 'a*int;; 
-     (* wartość, ile takich elementów *)
-
 exception Empty;;
 
 type 'a queue  = 
     Pusta |
-    Korzen of 'a queue * 'a node * 'a queue * int;; 
+    Korzen of 'a queue * 'a * 'a queue * int;; 
     (* lewy syn, ten element, prayw syn, długośc ścierzki do najbliższego liścia po prawej *)
 
 let empty = 
@@ -14,12 +10,12 @@ let empty =
 
 let is_empty = 
     function
-    | Pusta -> true
+    | Pusta -> true 
     | Korzen _ -> false
 
 let unpack = 
     function
-    | Korzen(lq,n,rq,rlp) -> lq,n,rq,rlp
+    | Korzen(lq,value,rq,rlp) -> lq,value,rq,rlp
     | Pusta -> assert false;;
 
 let rec join l r = 
@@ -27,53 +23,45 @@ let rec join l r =
     | Pusta, Pusta -> Pusta
     | Pusta, Korzen _ -> r
     | Korzen _, Pusta -> l
-        
-    (*1. Dodajemy jeden element który już jest, zwiększamy tylko count  *)
-    | Korzen (lleft,Node(lval,lcount),lright,lrlp),
-      Korzen (rleft,Node(rval,rcount),rright,rrlp) 
-      when lval = rval -> 
-        if lleft = Pusta && lright = Pusta
-          then Korzen(rleft,Node(rval,rcount+1),rright,rrlp)
-        else Korzen(lleft,Node(lval,lcount+1),lright,lrlp)
 
-    (*2. Dodajemy zupełnie nowy element *)
-            
-    (* a) Korzeń lewej mniejszy od korzenia prawej *)
-    (*2.1 a) Lewa nie ma prawego ani lewego syna, dołączamy r jako lewego syna i aktualizujemy l.rpl = r.rpl + 1  *)
-    | Korzen (lleft,Node(lval,lcount),lright,lrlp),
-      Korzen (rleft,Node(rval,rcount),rright,rrlp) 
-      when lval < rval && lleft = Pusta && lright = Pusta->
-        Korzen(r,Node(lval,lcount),Pusta,0)
 
-    (*2.2 a) Lewa ma lewego albo prawego syna  *)
-    | Korzen (lleft,Node(lval,lcount),lright,lrlp),
-      Korzen (rleft,Node(rval,rcount),rright,rrlp) 
-      when lval < rval ->
+    | Korzen (lleft,lval,lright,lrlp),
+      Korzen (rleft,rval,rright,rrlp) 
+      when lval <= rval && lleft = Pusta && lright = Pusta->
+        Korzen(r,lval,Pusta,0)
+
+    | Korzen (lleft,lval,lright,lrlp),
+      Korzen (rleft,rval,rright,rrlp) 
+      when lval <= rval ->
         let _,_,_,nowe_lewe_rlp = unpack lleft in
-        let Korzen(npleft,npnode,npright,nrlp)  = join lright r 
+        if lright = Pusta
+          then  
+            if rrlp <= nowe_lewe_rlp 
+              then Korzen(lleft,lval,r,rrlp)
+            else Korzen(r,lval,lleft,nowe_lewe_rlp+1)
+        else
+        let Korzen(npleft,npval,npright,nrlp)  = join lright r 
         in
         if nrlp <= nowe_lewe_rlp 
-          then Korzen(lleft,Node(lval,lcount),Korzen(npleft,npnode,npright,nrlp),nrlp +1)
+          then Korzen(lleft,lval,Korzen(npleft,npval,npright,nrlp),nrlp +1)
         else
-          Korzen(Korzen(npleft,npnode,npright,nrlp),Node(lval,lcount),lleft,nowe_lewe_rlp+1)
+          Korzen(Korzen(npleft,npval,npright,nrlp),lval,lleft,nowe_lewe_rlp+1)
       
     (* b) tak samo tylko zamieniamy lewą z prawą *)
-    | Korzen (lleft,Node(lval,lcount),lright,lrlp),
-      Korzen (rleft,Node(rval,rcount),rright,rrlp) ->
+    | Korzen (lleft,lval,lright,lrlp),
+      Korzen (rleft,rval,rright,rrlp) ->
       (* let a = print_int (3) in *)
       join r l;;
 
 let add e q = 
-  join (Korzen(Pusta,Node(e,1),Pusta,0)) q;;
+  join (Korzen(Pusta,e,Pusta,0)) q;;
 
 
 let delete_min q = 
   match q with
   | Pusta -> raise Empty
-  | _ -> let (l,n,r,rlp) = unpack q in
-    match n with
-    | Node(value,1) -> value, (join l r)
-    | Node(value,greater_than_one) -> value, Korzen(l,Node(value,greater_than_one-1),r,rlp);; 
+  | _ -> let (l,n,r,rlp) = unpack q in n,(join l r);;
+    
 
         
 
