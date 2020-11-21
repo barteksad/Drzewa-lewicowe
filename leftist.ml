08f617956022ffc7f97c4b4c15d78c9a9a622a65
barteksad <barteksadlej@gmail.com>
2020-11-21 14:46:59 +0100

    working version but shorter

--- leftist.ml
+++ leftist.ml
@@ -1,3 +1,6 @@
+(* Zadanie 2 - Drzewa lewicowe *)
+(* Autor: Bartek Sadlej *)
+
 exception Empty;;
 
 type 'a queue  = 
@@ -13,6 +16,7 @@ let is_empty =
     | Pusta -> true 
     | Korzen _ -> false
 
+(* funkcja pomocnicza biorąca Korzeń i zwracająca jego elementy jako krotke *)
 let unpack = 
     function
     | Korzen(lq,value,rq,rlp) -> lq,value,rq,rlp
@@ -20,34 +24,41 @@ let unpack =
 
 let rec join l r = 
     match l,r with
-    | Pusta, Pusta -> Pusta
-    | Pusta, Korzen _ -> r
-    | Korzen _, Pusta -> l
+    | Pusta, Pusta -> Pusta (*  Puste nie ma co łączyć*)
+    | Pusta, Korzen _ -> r (*  Puste  i kolejka zwracamy kolejke*)
+    | Korzen _, Pusta -> l (*  Puste  i kolejka zwracamy kolejke*)
 
+    (* ------------ Łączenie nie pustych ------------ *)
+    (* chcemy żeby wartośc w korzeniu lewej była <= niż w kożeniu prawej *)
+    (* jeśli tak nie jest wywołujemy join od prawej i lewej, wtedy się zamienia *)
 
+    (* 1. Jeśli w lewej jest jedne element to jako lewe poddrzewo lewej ustawiamy prawą i załatwione *)
     | Korzen (lleft,lval,lright,lrlp),
       Korzen (rleft,rval,rright,rrlp) 
       when lval <= rval && lleft = Pusta && lright = Pusta->
         Korzen(r,lval,Pusta,0)
 
+    (* 2. Lewa ma jedno lub dwa poddrzewo *)
     | Korzen (lleft,lval,lright,lrlp),
       Korzen (rleft,rval,rright,rrlp) 
       when lval <= rval ->
+
+        (* nowe_lewe_rlp = długość najkrótszej prawostronnej ścieżki do liścia w lewym poddrzewie lewej *)
         let _,_,_,nowe_lewe_rlp = unpack lleft in
-        if lright = Pusta
-          then  
-            if rrlp <= nowe_lewe_rlp 
-              then Korzen(lleft,lval,r,rrlp)
-            else Korzen(r,lval,lleft,nowe_lewe_rlp+1)
-        else
-        let Korzen(npleft,npval,npright,nrlp)  = join lright r 
+
+        (* Łączymy prawe poddrzewo lewej z prawą *)
+
+        (* jeśli ścieżka w nowej kolejce jest dłuższa niż w lewym poddrzewie lewj *)
+        (* to zamieniammy te elementy ze sobą żeby zachować lewicowość *)
+        let Korzen(npleft,npval,npright,nowe_prawe_rlp)  = join lright r 
         in
-        if nrlp <= nowe_lewe_rlp 
-          then Korzen(lleft,lval,Korzen(npleft,npval,npright,nrlp),nrlp +1)
+        if nowe_prawe_rlp <= nowe_lewe_rlp 
+          then Korzen(lleft,lval,Korzen(npleft,npval,npright,nowe_prawe_rlp),nowe_prawe_rlp +1)
         else
-          Korzen(Korzen(npleft,npval,npright,nrlp),lval,lleft,nowe_lewe_rlp+1)
+          Korzen(Korzen(npleft,npval,npright,nowe_prawe_rlp),lval,lleft,nowe_lewe_rlp+1)
       
-    (* b) tak samo tylko zamieniamy lewą z prawą *)
+    (* Wartość w korzeniu lewej jest większa niż w prawej *)
+    (* więc wywołujemy join od prawej jako lewa i lewej jako prawa*)
     | Korzen (lleft,lval,lright,lrlp),
       Korzen (rleft,rval,rright,rrlp) ->
       join r l;;
